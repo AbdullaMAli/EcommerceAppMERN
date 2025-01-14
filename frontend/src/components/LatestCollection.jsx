4b65d74c15fc96d952d2b290d63ed9eb845e7b10
AbdullaMAli <abdullaaliyar6@gmail.com>
2025-01-14 13:28:11 +0530

    navbar completed,even made it responsive. Then completed hero section with imported google font

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,14 @@
+import React, {useContext} from 'react'
+import { ShopContext } from '../context/ShopContext';
+function LatestCollection() {
+
+    const {products} = useContext(ShopContext);
+    console.log(products);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LatestCollection
